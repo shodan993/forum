1536cee2ab82272ede0100d85c0aa41af997b698
shodan993 <paulnstanley@gmail.com>
2018-07-30 15:56:38 -0400

    working on button functionality

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-// your code here
+var button = document.getElementById('button');
+
+button.addEventListener('click', function () {
+  var message = document.getElementById('name').value;
+  var messageList = document.createElement('li');
+  var userMessage = document.createTextNode(message);
+  messageList.appendChild(userMessage);
+  document.getElementsByClassName('posts')[0].append(messageList);
+});
